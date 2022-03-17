88fc15d3cf2347cf42de26c566d91386d6f8e46e
karpova.m <34@3389.ru>
2022-03-17 14:58:08 +0700

    Реализовано получение категорийи и всего списка задач в стиле РП (реактивное программирование)

--- src/app/services/data-handler.service.ts
+++ src/app/services/data-handler.service.ts
@@ -2,21 +2,22 @@ import {Injectable} from '@angular/core';
 import {Category} from "../model/Category";
 import {Task} from "../model/Task";
 import {TestData} from "../data/TestData";
-import {Subject} from "rxjs";
+import {BehaviorSubject, Subject} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataHandlerService {
 
-  tasksSubject = new Subject<Task[]>()
+  tasksSubject = new BehaviorSubject<Task[]>(TestData.tasks)
+  categoriesSubject = new BehaviorSubject<Category[]>(TestData.categories)
 
   constructor() {
   }
 
-  public getCategories(): Category[] {
-    return TestData.categories
-  }
+  // public getCategories() {
+  //   this.categoriesSubject.next(TestData.categories)
+  // }
 
   public fillTasks() {
     this.tasksSubject.next(TestData.tasks)
