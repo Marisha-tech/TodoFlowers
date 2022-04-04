40654bdcaee77e0ac4b00a3e4936f9d71b22528f
karpova.m <34@3389.ru>
2022-04-04 12:46:16 +0700

    Подключение диалогового окна. Работа с диалоговым окном при клике на задачу

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,22 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatTableModule} from "@angular/material/table";
 import {MatSortModule} from "@angular/material/sort";
 import {MatPaginatorModule} from "@angular/material/paginator";
+import {MatDialogModule} from "@angular/material/dialog";
+import {CdkTableModule} from "@angular/cdk/table";
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {CategoriesComponent} from './views/categories/categories.component';
 import {TasksComponent} from './views/tasks/tasks.component';
-import {CdkTableModule} from "@angular/cdk/table";
+import {EditTaskDialogComponent} from './dialog/edit-task-dialog/edit-task-dialog.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     CategoriesComponent,
-    TasksComponent
+    TasksComponent,
+    EditTaskDialogComponent
   ],
   imports: [
     BrowserModule,
@@ -26,9 +29,11 @@ import {CdkTableModule} from "@angular/cdk/table";
     MatTableModule,
     MatSortModule,
     MatPaginatorModule,
-    CdkTableModule
+    CdkTableModule,
+    MatDialogModule
   ],
   providers: [],
+  entryComponents: [EditTaskDialogComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule {
