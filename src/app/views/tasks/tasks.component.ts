6444665cf45735b5d8a92c62c8f47db5fc85ffc2
karpova.m <34@3389.ru>
2022-04-05 17:07:51 +0700

    Реализован переход в категорию из общего списка задач

--- src/app/views/tasks/tasks.component.ts
+++ src/app/views/tasks/tasks.component.ts
@@ -16,6 +16,7 @@ import {MatSort} from "@angular/material/sort";
 import {MatDialog} from "@angular/material/dialog";
 import {EditTaskDialogComponent} from "../../dialog/edit-task-dialog/edit-task-dialog.component";
 import {ConfirmDialogComponent} from "../../dialog/confirm-dialog/confirm-dialog.component";
+import {Category} from "../../model/Category";
 
 @Component({
   selector: 'app-tasks',
@@ -46,6 +47,11 @@ export class TasksComponent implements OnInit {
 
   @Output()
   updateTask = new EventEmitter<Task>()
+
+  @Output()
+  selectCategory = new EventEmitter<Category>() //нажали на категорию из списка задач
+
+  selectedCategory: Category
 
   constructor(
     private dataHandler: DataHandlerService, // доступ к данным
@@ -175,5 +181,9 @@ export class TasksComponent implements OnInit {
   onToggleStatus(task: Task) {
     task.completed = !task.completed
     this.updateTask.emit(task)
+  }
+
+  onSelectCategory(category: Category): void {
+    this.selectCategory.emit(category)
   }
 }
