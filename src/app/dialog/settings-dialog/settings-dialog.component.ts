b9aded8110f5aa5484ada44531105ed4d1170a5b
karpova.m <34@3389.ru>
2022-04-15 17:35:24 +0700

    Приоритеты и цвета

---
+++ src/app/dialog/settings-dialog/settings-dialog.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import {Priority} from "../../model/Priority";
+import {MatDialogRef} from "@angular/material/dialog";
+import {DataHandlerService} from "../../services/data-handler.service";
+
+@Component({
+  selector: 'app-settings-dialog',
+  templateUrl: './settings-dialog.component.html',
+  styleUrls: ['./settings-dialog.component.scss']
+})
+export class SettingsDialogComponent implements OnInit {
+
+  public priorities: Priority[]
+
+  constructor(
+    private dialogRef: MatDialogRef<SettingsDialogComponent>, // для возможности работы с текущим диалоговым окном
+    private dataHandler: DataHandlerService // ссылка на сервис для работы с данными
+  ) { }
+
+  ngOnInit(): void {
+    //получаем все значения, чтобы отобразить настройку цветов
+    this.dataHandler.getAllPriorities().subscribe(priorities => this.priorities = priorities)
+  }
+
+  // кнопка закрыть окно
+  onClose() {
+    this.dialogRef.close(false)
+  }
+}
